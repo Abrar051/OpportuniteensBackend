7226e30cb6e1e7d3ab90209752cc503dff69cb19
Abrar Shahriar <75354845+Abrar051@users.noreply.github.com>
2022-05-16 21:07:46 -0700

    Update job_portal.sql

--- job_portal.sql
+++ job_portal.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 06, 2021 at 03:04 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.0.33
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
